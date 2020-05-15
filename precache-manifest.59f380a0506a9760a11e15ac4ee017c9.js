4e0e63032c18cf8dbe840617bba1f8f292ac09d8
Admin2 <admin2@Admins-MacBook-Air.local>
2020-05-15 16:52:07 -0400

    Updates

---
+++ precache-manifest.59f380a0506a9760a11e15ac4ee017c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c83e442d80746c47ce7671ee46de211",
+    "url": "/WhatToDo/index.html"
+  },
+  {
+    "revision": "8538507fa9990c615525",
+    "url": "/WhatToDo/static/css/main.50f110e8.chunk.css"
+  },
+  {
+    "revision": "4548c4a13c9c437d4fe7",
+    "url": "/WhatToDo/static/js/2.6dcc3260.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WhatToDo/static/js/2.6dcc3260.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8538507fa9990c615525",
+    "url": "/WhatToDo/static/js/main.7f8246ec.chunk.js"
+  },
+  {
+    "revision": "b9a70c9d6f6e8d572118",
+    "url": "/WhatToDo/static/js/runtime-main.8f0381b4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/WhatToDo/static/media/logo.5d5d9eef.svg"
+  }
+]);
